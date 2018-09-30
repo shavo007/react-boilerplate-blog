4000a4361fd5307e4dda210ec934c70184a087d3
shane lee <shanelee007@gmail.com>
2018-09-30 23:06:45 +1000

    snapshot

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,5 @@ import React from "react";
 it("should match snapshot", () => {
   const wrapper = shallow(<App />);
 
-  // expect(wrapper.find("h1").text()).toBe("Showcase code splitting");
-  expect(wrapper).toMatchSnapshot;
+  expect(wrapper).toMatchSnapshot();
 });
